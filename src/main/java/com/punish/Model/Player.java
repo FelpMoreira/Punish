88b24833f2818dfee777cee4a669bba349f5e5a2
Felp <wtffelp@gmail.com>
2026-07-02 22:02:13 -0300

    feat: add Player constructor with id parameter

--- src/main/java/com/punish/Model/Player.java
+++ src/main/java/com/punish/Model/Player.java
@@ -6,6 +6,8 @@ public class Player {
 
     public Player () {}
 
+    public Player (Long id) { this.id = id; }
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
