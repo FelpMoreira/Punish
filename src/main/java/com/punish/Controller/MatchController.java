36963fe70c79b965b9d46bc923918d0992e1447e
Felp <wtffelp@gmail.com>
2026-08-26 17:37:00 -0300

    feat: add GET /matches with playerId and tournamentId filters

--- src/main/java/com/punish/Controller/MatchController.java
+++ src/main/java/com/punish/Controller/MatchController.java
@@ -18,6 +18,25 @@ public class MatchController {
             ctx.json(m);
         });
 
+        app.get("/matches", ctx -> {
+            String playerIdStr = ctx.queryParam("playerId");
+            String tournamentIdStr = ctx.queryParam("tournamentId");
+            String pageStr = ctx.queryParam("page");
+            String sizeStr = ctx.queryParam("size");
+            Long playerId = playerIdStr != null ? Long.parseLong(playerIdStr) : null;
+            Long tournamentId = tournamentIdStr != null? Long.parseLong(tournamentIdStr) : null;
+            if (pageStr != null && sizeStr != null) {
+                int page = Integer.parseInt(pageStr);
+                int size = Integer.parseInt(sizeStr);
+                var result = matchService.buscarHistorico(playerId, tournamentId, page, size);
+                ctx.json(result);
+            } else {
+                List<Match> matches = matchService.buscarHistorico(playerId, tournamentId, 1, 100).data();
+                ctx.json(matches);
+            }
+            
+        });
+
         app.get("/matches/{id}", ctx -> {
             Long match_id = Long.parseLong(ctx.pathParam("id"));
             Match m = matchService.buscarPorId(match_id);
