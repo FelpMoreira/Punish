b576f10283c8e5a8c26d83edae701d51729c8c74
Felp <wtffelp@gmail.com>
2026-08-31 15:53:31 -0300

    feat: add email, password_hash and role fields to Player model

--- src/main/java/com/punish/Model/Player.java
+++ src/main/java/com/punish/Model/Player.java
@@ -3,6 +3,9 @@ package com.punish.Model;
 public class Player {
     private Long id;
     private String nickname;
+    private String email;
+    private String password_hash;
+    private String role;
 
     public Player () {}
 
@@ -13,4 +16,14 @@ public class Player {
 
     public String getNickname() { return nickname; }
     public void setNickname(String nickname) { this.nickname = nickname; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword_hash() { return password_hash; }
+    public void setPassword_hash(String password_hash) { this.password_hash = password_hash; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
 }
