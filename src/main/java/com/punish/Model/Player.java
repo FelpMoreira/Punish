5390e737b9fd4d318b5a4a31eb65ff3d821cb4f0
Felp <wtffelp@gmail.com>
2026-08-31 16:59:54 -0300

    fix: hide password_hash in JSON responses

--- src/main/java/com/punish/Model/Player.java
+++ src/main/java/com/punish/Model/Player.java
@@ -20,6 +20,7 @@ public class Player {
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
+    @com.fasterxml.jackson.annotation.JsonIgnore
     public String getPassword_hash() { return password_hash; }
     public void setPassword_hash(String password_hash) { this.password_hash = password_hash; }
 
