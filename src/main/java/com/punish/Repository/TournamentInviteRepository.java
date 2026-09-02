5a658a02024db468384c6895ab8be5002c9304e8
Felp <wtffelp@gmail.com>
2026-09-02 15:29:08 -0300

    feat: add invite CRUD with code lookup and usage tracking

---
+++ src/main/java/com/punish/Repository/TournamentInviteRepository.java
@@ -0,0 +1,63 @@
+package com.punish.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.jdbi.v3.core.Jdbi;
+
+import com.punish.Config.Database;
+import com.punish.Model.TournamentInvite;
+
+public class TournamentInviteRepository {
+    Jdbi jdbi = Database.getJdbi();
+
+    public TournamentInvite criar(Long fk_tounament_id, String codigo, Timestamp expira_em, Integer usos_max) {
+        Long id = jdbi.withHandle(handle -> {
+            return handle.createUpdate("INSERT INTO tournament_invite (fk_tournament_id, codigo, expira_em, usos_max) VALUES (:fk_tournament_id, :codigo, :expira_em, :usos_max)")
+            .bind("fk_tournament_id", fk_tounament_id)
+            .bind("codigo", codigo)
+            .bind("expira_em", expira_em)
+            .bind("usos_max", usos_max)
+            .executeAndReturnGeneratedKeys("id")
+            .mapTo(Long.class)
+            .findOne()
+            .orElse(null); 
+        });
+        return buscarPorCodigo(codigo);
+    }
+
+    public TournamentInvite buscarPorCodigo(String codigo) {
+        return jdbi.withHandle(handle -> 
+            handle.createQuery("SELECT * FROM tournament_invite WHERE codigo = :codigo")
+            .bind("codigo", codigo)
+            .mapToBean(TournamentInvite.class)
+            .findOne()
+            .orElse(null)
+        );
+    }
+
+    public List<TournamentInvite> buscarPorTournament(Long fk_tournament_id){
+        return jdbi.withHandle(handle -> 
+            handle.createQuery("SELECT * FROM tournament_invite WHERE fk_tournament_id = :fk_tournament_id ORDER BY criado_em DESC")
+            .bind("fk_tournament_id", fk_tournament_id)
+            .mapToBean(TournamentInvite.class)
+            .list()
+        );
+    }
+
+    public void incrementarUsos(Long id){
+        jdbi.withHandle(handle -> 
+            handle.createUpdate("UPDATE tournament_invites SET usos = usos + 1 WHERE id = :id")
+            .bind("id", id)
+            .execute()
+        );
+    }
+
+    public void deletar(Long id){
+        jdbi.withHandle(handle -> 
+            handle.createUpdate("DELETE FROM tournament_invite WHERE id = :id")
+            .bind("id", id)
+            .execute()
+        );
+    }
+}
