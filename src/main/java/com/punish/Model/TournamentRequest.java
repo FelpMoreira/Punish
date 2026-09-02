d3e0b1f3a821fa61f7d1e00cf83e5baca5988c13
Felp <wtffelp@gmail.com>
2026-09-02 13:46:59 -0300

    feat: add TournamentRequest model

---
+++ src/main/java/com/punish/Model/TournamentRequest.java
@@ -0,0 +1,29 @@
+package com.punish.Model;
+
+import java.security.Timestamp;
+
+public class TournamentRequest {
+    private Long id;
+    private Long fk_tournament_id;
+    private Long fk_player_id;
+    private String status;
+    private Timestamp criado_em;
+
+    public TournamentRequest() {}
+
+    public Long getId() { return id; }
+    public void setid(Long id) { this.id = id; }
+
+    public Long getFk_tournament_id() { return fk_tournament_id; }
+    public void setFk_tournament_id(Long fk_tournament_id) { this.fk_tournament_id = fk_tournament_id; }
+
+    public Long getFk_player_id() { return fk_player_id; }
+    public void setFk_player_id(Long fk_player_id) { this.fk_player_id = fk_player_id; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public Timestamp getCriado_em() { return criado_em; }
+    public void setCriado_em(Timestamp criado_em) { this.criado_em = criado_em; }
+    
+}
